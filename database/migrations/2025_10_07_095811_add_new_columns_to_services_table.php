b5019d9e23df71bddba1bd20ad2b28202549a9f2
chetan <chetanshelake147@gmail.com>
2025-10-07 14:37:28 +0530

    added migration file

---
+++ database/migrations/2025_10_07_095811_add_new_columns_to_services_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('services', function (Blueprint $table) {
+            // Add new columns
+            $table->text('short_description')->nullable()->after('description');
+            $table->json('whats_include')->nullable()->after('description');
+
+            // One-time price and description
+            $table->decimal('price_onetime', 10, 2)->nullable();
+            $table->text('price_onetime_desc')->nullable()->after('price_onetime');
+
+            // Weekly price and description
+            $table->decimal('price_weekly', 10, 2)->nullable()->after('price_onetime_desc');
+            $table->text('price_weekly_desc')->nullable()->after('price_weekly');
+
+            // Monthly price and description
+            $table->decimal('price_monthly', 10, 2)->nullable()->after('price_weekly_desc');
+            $table->text('price_monthly_desc')->nullable()->after('price_monthly');
+
+            // Yearly price and description
+            $table->decimal('price_yearly', 10, 2)->nullable()->after('price_monthly_desc');
+            $table->text('price_yearly_desc')->nullable()->after('price_yearly');
+
+            $table->boolean('is_arabic')->default(false)->after('price_yearly_desc');
+
+           });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('services', function (Blueprint $table) {
+            // Reverse the column rename first
+             // $table->renameColumn('price_onetime', 'price');
+
+            // Drop the added columns
+            $table->dropColumn([
+                'short_description',
+                'whats_include',
+                'price_onetime',
+                'price_onetime_desc',
+                'price_weekly',
+                'price_weekly_desc',
+                'price_monthly',
+                'price_monthly_desc',
+                'price_yearly',
+                'price_yearly_desc',
+                'is_arabic'
+            ]);
+        });
+    }
+};
